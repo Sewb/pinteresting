292954ee37ecb839a663717cdda5e92345745a57
Sewb <stevensalim@hotmail.com>
2014-11-14 14:01:25 +1100

    adding devise

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,4 +75,8 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # Required for Heroku
+  # Note set this to your actual hosting
+  config.action_mailer.default_url_options = { host: 'myownpinter.herokuapp.com' }
 end
